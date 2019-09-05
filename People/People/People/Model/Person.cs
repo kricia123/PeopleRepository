0f81698e7e2d2476139a147c1f3cb4b8be377934
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-09-05 08:42:10 -0600

    Table

---
+++ People/People/People/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace People.Model
+{
+    [Table("T_People)" ]
+    public class Person
+
+
+    {
+        //Miembro Properties
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+
+        [MaxLength(50), Unique]
+
+    public string Name { get; set; }
+
+    }
+}
