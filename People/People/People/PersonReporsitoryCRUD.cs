0313b7c54f70e47f92f3f3b42d1a61ca189ffda1
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-09-10 09:15:53 -0600

    MainPageRe

--- People/People/People/PersonReporsitoryCRUD.cs
+++ People/People/People/PersonReporsitoryCRUD.cs
@@ -8,17 +8,86 @@ using People.Model;
 namespace People
 {
     public class PersonReporsitoryCRUD
+
+
     {
-      SQLiteConnection conn;
+        public class PersonRepositoryCRUD
+        {
+            SQLiteConnection conn;
 
-        public string StatusMessage { get; set; }
+            // conn = new SQLiteConnection
+            public string StatusMessage { get; set; }
 
-        public PersonReporsitoryCRUD(string dbpath) {
-            conn = new SQLiteConnection(dbpath);
-            conn.CreateTable<Person>();
+            // Constructor
+            public PersonRepositoryCRUD(string dbPath)
+            {
+                // Creamos la conexión.
+                conn = new SQLiteConnection(dbPath);
+                conn.CreateTable<Person>();
+
+                /* PersonRepositoryCRUD Repo = 
+                new PersonRepositoryCRUD("c:\dev\datos");*/
+            }
+
+            // CRUD OPERATIONS o métodos
+            // cREAR
+
+            /* Tipos de datos de retorno:
+             *  void = Vacío
+             *  int = Entero
+             *  string = Cadena
+             *  Decimal = decimal
+             *  Person = un objeto de tipo Peson.
+            */
+            public void CreatePerson(Person newPerson)
+            {
+
+                int result;
+                result = conn.Insert(newPerson);
+                if (result == 1)
+                {
+                    StatusMessage =
+                        $"{result} registro agregado [Nombre: " +
+                        $"{newPerson.Name}, ID:{newPerson.Id}";
+                }
+            }
 
         }
 
+        class Car
+        {
+            // Catacterísticas o Propiedades
+            public int NPlaca { get; set; }
+            public string Modelo { get; set; }
 
+            // Métodos acciones
+            void Arrancar()
+            {
+                //
+            }
+            void Avanzar()
+            {
+                //
+            }
+
+        }
     }
+
+    /* int resul = 0;
+         resul=Conn Insert(newPerson);
+     if(resul==1)
+
+ }
+
+ {
+
+ StatusMessage= $"{resul} registros agregados [Nombre:" + $"{NewPerson.Name}, ID:{newPerson.id}]";
+
+ }
+     {
+     else
+     }
+
+     {
+    statusMessenger= $" registro no agregado";*/
 }
